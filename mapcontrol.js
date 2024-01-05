3e21b1df7021eb926580a851e706f07363e3d8a5
EthanBassingthwaite <114631738+EthanBassingthwaite@users.noreply.github.com>
2024-01-05 10:39:05 -0700

    Update mapcontrol.js

--- mapcontrol.js
+++ mapcontrol.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
   alert('Ready to go');
 
-  $("place1").mouseover(function(){
+  $("#place1").mouseover(function(){
     $("p").css("background-color", "yellow");
   });
-  $("place1").mouseout(function(){
+  $("#place1").mouseout(function(){
     $("p").css("background-color", "lightgray");
   });
   
