454572ccbfac658b0cd59462233cb2c234b24c38
EthanBassingthwaite <114631738+EthanBassingthwaite@users.noreply.github.com>
2024-01-05 09:56:04 -0700

    Update mapcontrol.js

--- mapcontrol.js
+++ mapcontrol.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
   alert('Ready to go');
+
+  $("p").mouseover(function(){
+    $("p").css("background-color", "yellow");
+  });
+  $("p").mouseout(function(){
+    $("p").css("background-color", "lightgray");
+  });
+  
+});
   
 });
