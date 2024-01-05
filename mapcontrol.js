c0d2b3e016c3ecdeabb41b949882ceb15289539a
EthanBassingthwaite <114631738+EthanBassingthwaite@users.noreply.github.com>
2024-01-05 11:19:59 -0700

    Update mapcontrol.js

--- mapcontrol.js
+++ mapcontrol.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   alert('Ready to go5');
 
-  $("#place1").mouseover(function(){
+  $("#place1icon").mouseover(function(){
     $('#place1").addclass('visible');
     $('#place1").removeclass('invisible');
   });
-  $("#place1").mouseout(function(){
+  $("#place1icon").mouseout(function(){
     $('#place1').addclass('invisible');
     $('#place1').removeclass('visible');
     alert('wow');
